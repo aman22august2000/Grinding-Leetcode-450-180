e007b79ccd237f4d5d43acaf72456b87d991cb5a
Aman Kumar Gupta <54740374+aman22august2000@users.noreply.github.com>
2022-03-09 16:18:10 +0530

    Time: 2 ms (62.29%), Space: 44.3 MB (49.28%) - LeetHub

--- 1528-shuffle-string/1528-shuffle-string.java
+++ 1528-shuffle-string/1528-shuffle-string.java
@@ -1,10 +1,10 @@
 class Solution {
     public String restoreString(String s, int[] indices) {
-      char ch[]=new char[indices.length];
-      for(int i=0;i<indices.length;i++)
+      StringBuilder res=new StringBuilder(s);
+      for(int i=0;i<s.length();i++)
       {
-        ch[indices[i]]=s.charAt(i);
+        res.setCharAt(indices[i],s.charAt(i));
       }
-      return new String(ch);
+      return res.toString();
     }
 }
