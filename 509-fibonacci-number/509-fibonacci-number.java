976f589683b5844f2c7dcd69a352b93e4a7e075e
Aman Kumar Gupta <54740374+aman22august2000@users.noreply.github.com>
2022-04-22 19:43:42 +0530

    Time: 8 ms (33.82%), Space: 41 MB (42.75%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.java
+++ 509-fibonacci-number/509-fibonacci-number.java
@@ -1,6 +1,7 @@
 class Solution {
     public int fib(int n) {
-        double sqrt5 = Math.sqrt(5);
-        return (int)((Math.pow(1 + sqrt5, n) - Math.pow(1 - sqrt5, n)) / (double)Math.pow(2, n) / sqrt5);
-    }
+        if(n<=1)
+          return n;    
+      return fib(n-1)+fib(n-2);
+    }  
 }
