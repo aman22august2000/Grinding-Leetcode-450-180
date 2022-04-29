0ee697698f49d39dbc6ac395afd01818c237c47e
Aman Kumar Gupta <54740374+aman22august2000@users.noreply.github.com>
2022-04-29 23:00:36 +0530

    Time: 0 ms (100.00%), Space: 54.1 MB (23.47%) - LeetHub

--- 785-is-graph-bipartite/785-is-graph-bipartite.java
+++ 785-is-graph-bipartite/785-is-graph-bipartite.java
@@ -1,30 +1,20 @@
 class Solution {
     public boolean isBipartite(int[][] graph) {
-      var color =new int[graph.length];
-      Arrays.fill(color,-1);
+      var colors =new int[graph.length];
       
       for(var i=0;i<graph.length;i++)
-        if(color[i]==-1 && !bfs(i,graph,color))
+        if(colors[i]==0 && !dfs(graph,colors,1,i))
           return false;
       return true;
     }
-  private boolean bfs(int node,int [][] graph,int[] color){
-    var q=new ArrayDeque<>(List.of(node));
-    color[node]=0;
+  private boolean dfs(int [][] graph,int[] colors,int color, int node){
+    if(colors[node] != 0)
+      return colors[node] == color;
+    colors[node]=color;
     
-    while(!q.isEmpty())
-    {
-      var curr=q.poll();
-      for(var neighbor : graph[curr])
-      {
-        if(color[neighbor] == color[curr])
-          return false;
-        if(color[neighbor] == -1){
-          color[neighbor] = 1 - color[curr];
-          q.add(neighbor);
-        }
-      }
-    }
-    return true;
+    for(var nei : graph[node])
+      if(!dfs(graph,colors,-color,nei))
+        return false;
+      return true;
   }
 }
