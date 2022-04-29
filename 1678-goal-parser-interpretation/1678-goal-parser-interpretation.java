dfb7e1a401965d9678a6717bc205e1ba7a6afdd9
Aman Kumar Gupta <54740374+aman22august2000@users.noreply.github.com>
2022-04-30 02:57:35 +0530

    Time: 1 ms (82.34%), Space: 42.5 MB (26.07%) - LeetHub

--- 1678-goal-parser-interpretation/1678-goal-parser-interpretation.java
+++ 1678-goal-parser-interpretation/1678-goal-parser-interpretation.java
@@ -1,5 +1,23 @@
 class Solution {
-    public String interpret(String command) {
-        return command.replace("()","o").replace("(al)","al");
+    public String interpret(String str) {
+      StringBuilder s = new StringBuilder(str.length());
+        for(int i=0;i<str.length();i++)
+        {
+          if(str.charAt(i)=='G') s.append('G');
+          if(str.charAt(i)=='(')
+          {
+            if(str.charAt(i+1)==')')
+            {
+              s.append('o');
+              i++;
+            }
+            else
+            {
+              s.append("al");
+              i+=3;
+            }
+          }
+        }
+      return s.toString();
     }
 }
