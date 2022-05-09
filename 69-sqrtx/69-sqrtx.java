1d7a985d4a342a4e90e6081d5b2a8667efcab8f4
Aman Kumar Gupta <54740374+aman22august2000@users.noreply.github.com>
2022-05-09 15:42:20 +0530

    Time: 1 ms (99.24%), Space: 39.3 MB (96.47%) - LeetHub

--- 69-sqrtx/69-sqrtx.java
+++ 69-sqrtx/69-sqrtx.java
@@ -1,16 +1,10 @@
 class Solution {
     public int mySqrt(int x) {
-        if(x==0) return 0;
-      int lo=1,hi=Integer.MAX_VALUE;
-      while(true){
-        int mid=lo+(hi-lo)/2;
-        if(mid>x/mid)
-          hi=mid-1;
-        else{
-          if(mid+1>x/(mid+1))
-            return mid;
-          lo=mid+1;
-        }
+        long r=x;
+      while(r*r>x)
+      {
+        r=(r+x/r)/2;
       }
+      return (int) r;
     }
 }
