896649f2f89f199090a6d3e383a1e846f4bd03da
Aman Kumar Gupta <54740374+aman22august2000@users.noreply.github.com>
2022-05-12 15:13:47 +0530

    Time: 4 ms (65.09%), Space: 47.4 MB (69.26%) - LeetHub

---
+++ 47-permutations-ii/47-permutations-ii.java
@@ -0,0 +1,24 @@
+class Solution {
+    public void traverse(List<List<Integer>> resultList, List<Integer> current, int[] nums, boolean[] used) {
+	if( current.size() == nums.length )
+		resultList.add(new ArrayList<Integer>(current));
+	else{
+		for(int i = 0; i < nums.length; i++) {
+			if( used[i] || ( i > 0 && nums[i] == nums[i - 1] && !used[i - 1]) ) // remove duplicate
+				continue;
+			current.add(nums[i]);
+			used[i] = true;
+			traverse(resultList, current, nums, used);
+			used[i] = false;
+			current.remove(current.size() - 1);
+		}
+	}
+}
+
+public List<List<Integer>> permuteUnique(int[] nums) {
+	Arrays.sort(nums); // sort is necessary
+	List<List<Integer>> resultList = new ArrayList<List<Integer>>();
+	traverse(resultList, new ArrayList<Integer>(), nums, new boolean[nums.length]);
+	return resultList;
+}
+}
