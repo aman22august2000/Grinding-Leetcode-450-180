a3e002a503ed6e71092ad6caa1f6f69c4ab2448f
Aman Kumar Gupta <54740374+aman22august2000@users.noreply.github.com>
2022-05-14 13:18:51 +0530

    Time: 0 ms (100.00%), Space: 41.7 MB (97.48%) - LeetHub

--- 74-search-a-2d-matrix/74-search-a-2d-matrix.java
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.java
@@ -1,29 +1,13 @@
-class Solution 
-{
-    public boolean searchMatrix(int[][] a, int t) 
-    {
-       //  int flag=0;
-       //  for(int i=1;i<a.length;i++)
-       //  {
-       //      if(t>=a[i-1][0] && t<=a[i][0])
-       //      {
-       //          for(int j=0;j<a[i].length;j++)
-       //          {
-       //              if(a[i][j]==t)
-       //              flag=1;
-       //          }
-       //      }
-       //  }
-       // if(flag==1)
-       //     return true;
-       //  else
-       //      return false;
-      
-      for(int i=0;i<a.length;i++)
-      {
-        for(int j=0;j<a[0].length;j++)
-          if(a[i][j]==t)
-            return true;
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+      int i = 0, j = matrix[0].length - 1;
+      while(i < matrix.length && j >= 0) {
+        if(matrix[i][j] == target)
+          return true;
+        else if(matrix[i][j] > target)
+          j --;
+        else if(matrix[i][j] < target)
+          i ++;
       }
       return false;
     }
