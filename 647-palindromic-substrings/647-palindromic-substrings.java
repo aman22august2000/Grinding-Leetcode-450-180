244592752cf225b136561732bd2fdebd7c7905a9
Aman Kumar Gupta <54740374+aman22august2000@users.noreply.github.com>
2022-05-22 23:49:40 +0530

    Time: 39 ms (16.57%), Space: 44.7 MB (19.33%) - LeetHub

--- 647-palindromic-substrings/647-palindromic-substrings.java
+++ 647-palindromic-substrings/647-palindromic-substrings.java
@@ -1,15 +1,45 @@
 class Solution {
     public int countSubstrings(String s) {
-        
-      int len=s.length(),ans=0;
-      for(int i=0;i<len;i++)
+      int l=s.length();
+      boolean dp[][]=new boolean [l][l];
+      for(int i=0;i<l;i++)
       {
-        int j=i-1,k=i;
-        while(k<len-1 && s.charAt(k)==s.charAt(k+1)) k++;
-        ans+=(k-j)*(k-j+1)/2;
-        i=k++;
-        while(j>=0 && k<len && s.charAt(j--)==s.charAt(k++)) ans++;
+        for(int j=0;j<l;j++)
+        {
+          dp[i][j]=false;
+        }
+      }      
+      for(int i=0;i<l;i++)
+      {
+        dp[i][i]=true;
       }
-      return ans;
+      for(int i=0;i<l-1;i++)
+      {
+        if(s.charAt(i)==s.charAt(i+1))
+          dp[i][i+1]=true;
+      }
+      
+      for(int gap=2;gap<l;gap++)
+      {
+        int i=0;
+        int j=gap;
+        while(i<l-gap || j<l)
+        {
+          if(s.charAt(i)==s.charAt(j) && dp[i+1][j-1]==true)
+            dp[i][j]=true;
+          i++;
+          j++;
+        }
+      }
+      int cnt=0;
+      for(int i=0;i<l;i++)
+      {
+        for(int j=0;j<l;j++)
+        {
+          if(dp[i][j]==true)
+            cnt++;
+        }
+      }
+      return cnt;
     }
 }
