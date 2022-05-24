cb3357744c1034e32138b884390e8a8f1fef9f4a
Aman Kumar Gupta <54740374+aman22august2000@users.noreply.github.com>
2022-05-24 20:29:46 +0530

    Time: 1 ms (100.00%), Space: 41.7 MB (96.39%) - LeetHub

--- 32-longest-valid-parentheses/32-longest-valid-parentheses.java
+++ 32-longest-valid-parentheses/32-longest-valid-parentheses.java
@@ -1,55 +1,18 @@
 class Solution {
     public int longestValidParentheses(String s) {
-      
-      int open=0,close=0;
-      int max=0;      
-      
-      // 0 -- n
-      for(int i=0;i<s.length();i++)
-      {
-        char c=s.charAt(i);
-        if(c=='(')
-        {
-          open++;
-        }
-        else
-        {
-          close++;
-        }
-        if(open==close)
-        {
-          int len=open+close; 
-          max=Math.max(max,len);
-        }
-        else if(close>open)
-        {
-          open=close=0;
+        int maxans = 0;
+      int dp[]=new int[s.length()];
+      for(int i=1;i<s.length();i++) {
+        if(s.charAt(i)==')') {
+          if(s.charAt(i-1)=='(') {
+            dp[i]=(i>=2 ? dp[i-2] : 0)+2;
+          } 
+          else if(i-dp[i-1]>0 && s.charAt(i-dp[i-1]-1)=='('){
+            dp[i]=dp[i-1]+((i - dp[i-1]) >= 2 ? dp[i-dp[i-1]-2]:0)+2;
+          }
+          maxans=Math.max(maxans,dp[i]);
         }
       }
-      
-      open = close = 0;
-      //n -- 0
-      for(int i=s.length()-1;i>=0;i--)
-      {
-        char c=s.charAt(i);
-        if(c=='(')
-        {
-          open++;
-        }
-        else
-        {
-          close++;
-        }
-        if(open==close)
-        {
-          int len=open+close; 
-          max=Math.max(max,len);
-        }
-        else if(open>close)
-        {
-          open=close=0;
-        }
-      }
-      return max;
+      return maxans;
     }
 }
