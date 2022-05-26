2c2ce23fbcf80e9bf3769f4c36c3d48c191731c9
Aman Kumar Gupta <54740374+aman22august2000@users.noreply.github.com>
2022-05-26 18:03:14 +0530

    Time: 1 ms (83.40%), Space: 43.1 MB (74.84%) - LeetHub

--- 31-next-permutation/31-next-permutation.java
+++ 31-next-permutation/31-next-permutation.java
@@ -1,27 +1,42 @@
 class Solution {
     public void nextPermutation(int[] nums) {
-      if(nums==null || nums.length<=1) return;
-      
-      int i=nums.length-2;
-      while(i>=0 && nums[i]>=nums[i+1]) i--;
-      if(i>=0)
+      int pivot=indexOfLastPeak(nums)-1;
+      if(pivot!=-1)
       {
-        int j=nums.length-1;
-        while(nums[j]<=nums[i]) j--;
-        swap(nums,i,j);
-      }
-      reverse(nums,i+1,nums.length-1);
+        int nextPrefix=lastIndexOfGreater(nums,nums[pivot]);
+        swap(nums,pivot,nextPrefix);                      
+      }   
+    reverse(nums,pivot+1);         
     }
   
-  public void swap(int nums[],int i,int j)
+  int indexOfLastPeak(int nums[])
+  {
+    for(int i=nums.length-1;0<i;--i){
+      if(nums[i-1]<nums[i]) return i;
+    }
+    return 0;
+  }
+  
+  int lastIndexOfGreater(int [] nums,int threshold)
+  {
+    for(int i=nums.length-1;0<=i;--i)
+    {
+      if(threshold<nums[i]) return i;
+    }
+    return -1;
+  }
+  
+  public void reverse(int [] nums,int i)
+  {
+    int j=nums.length-1;
+    while(i<j)
+      swap(nums,i++,j--);
+  }
+  
+  public void swap(int [] nums,int i, int j)
   {
     int temp=nums[i];
     nums[i]=nums[j];
     nums[j]=temp;
-  }
-  public void reverse(int nums[],int i,int j)
-  {
-    while(i<j) 
-      swap(nums,i++,j--);
-  }
+  }  
 }
