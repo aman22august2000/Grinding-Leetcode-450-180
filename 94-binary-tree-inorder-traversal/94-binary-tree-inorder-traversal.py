6247b9dffb667e241b96443bbb4555576670a9fb
Aman Kumar Gupta <54740374+aman22august2000@users.noreply.github.com>
2022-07-25 23:33:45 +0530

    Time: 42 ms (64.74%), Space: 13.8 MB (59.35%) - LeetHub

--- 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.py
+++ 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.py
@@ -1,34 +1,19 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
-class Solution {
-    public List<Integer> inorderTraversal(TreeNode root) {
-        List<Integer> ls=new ArrayList<Integer>();
-        Stack<TreeNode> st=new Stack<TreeNode>();
-        TreeNode curr=root;
-        while(curr!=null || !st.empty())
-        {
-            while(curr!=null)
-            {
-                st.add(curr);
-                curr=curr.left;
-            }
-            curr=st.pop();
-            ls.add(curr.val);
-            curr=curr.right;
-        }
-        return ls;
-    }
-}
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        res , stack = [] , []
+        while True:
+            while root:
+                stack.append(root)
+                root = root.left
+            if not stack:
+                return res
+            node = stack.pop()
+            res.append(node.val)
+            root = node.right
+        
