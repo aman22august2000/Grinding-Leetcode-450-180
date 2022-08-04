b352c67e122149a66087292e36b7c7ab6e9259e0
Aman Kumar Gupta <54740374+aman22august2000@users.noreply.github.com>
2022-08-04 14:28:49 +0530

    Time: 0 ms (100.00%), Space: 40.8 MB (69.44%) - LeetHub

--- 858-mirror-reflection/858-mirror-reflection.java
+++ 858-mirror-reflection/858-mirror-reflection.java
@@ -1,14 +1,5 @@
 class Solution {
     public int mirrorReflection(int p, int q) {
-        int ans=-1;
-        while(p%2==0 && q%2==0)
-        {
-            p/=2;
-            q/=2;
-        }
-        if(p%2==1 && q%2==0) ans=0;
-        if(p%2==0 && q%2==1) ans=2;
-        if(p%2==1 && q%2==1) ans=1;
-        return ans;
+        return (p & -p) > (q & -q) ? 2 : (p & -p) < (q & -q) ? 0:1;
     }
 }
