4d9c0a361f77977e4e143744776f56493fd81eae
Aman Kumar Gupta <54740374+aman22august2000@users.noreply.github.com>
2023-02-21 17:27:13 +0530

    Added solution - LeetHub

--- Check whether K-th bit is set or not - GFG/check-whether-kth-bit-is-set-or-not.java
+++ Check whether K-th bit is set or not - GFG/check-whether-kth-bit-is-set-or-not.java
@@ -15,7 +15,7 @@ class CheckBit
     static boolean checkKthBit(int n, int k)
     {
         // Your code here
-        if((n & (1<<k))!=0)
+        if(((n>>k) & 1)==1)
         return true;
         else
         return false;
