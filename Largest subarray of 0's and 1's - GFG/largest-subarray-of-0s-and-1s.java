9610c014b02bedcf8144adee9ca473e4540031b0
Aman Kumar Gupta <54740374+aman22august2000@users.noreply.github.com>
2023-04-01 13:33:12 +0530

    Added solution - LeetHub

--- Largest subarray of 0's and 1's - GFG/largest-subarray-of-0s-and-1s.java
+++ Largest subarray of 0's and 1's - GFG/largest-subarray-of-0s-and-1s.java
@@ -76,10 +76,10 @@ class Solution {
             hm.put(sum,i);
         }
         
-        for(int i=0;i<n;i++)
-        {
-            arr[i]=(arr[i]==-1)?0:1;
-        }
+        // for(int i=0;i<n;i++)
+        // {
+        //     arr[i]=(arr[i]==-1)?0:1;
+        // }
         
         return mx_len;
     }
