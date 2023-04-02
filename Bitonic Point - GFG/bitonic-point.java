968571a04ddcaf5172f2f7cef4feb004b0eb9131
Aman Kumar Gupta <54740374+aman22august2000@users.noreply.github.com>
2023-04-02 15:13:18 +0530

    Added solution - LeetHub

---
+++ Bitonic Point - GFG/bitonic-point.java
@@ -0,0 +1,68 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int tc = Integer.parseInt(br.readLine().trim());
+        while (tc-- > 0) {
+            String[] inputLine;
+            int n = Integer.parseInt(br.readLine().trim());
+            int[] arr = new int[n];
+            inputLine = br.readLine().trim().split(" ");
+            for (int i = 0; i < n; i++) {
+                arr[i] = Integer.parseInt(inputLine[i]);
+            }
+
+            int ans = new Solution().findMaximum(arr, n);
+            System.out.println(ans);
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+
+
+class Solution {
+    int findMaximum(int[] arr, int n) {
+        // code here
+        int lo=0,hi=n-1;
+        while(lo<=hi)
+        {
+            int mid=(lo+hi)/2;
+            if(mid==0)
+            {
+                if(arr[mid]>arr[mid+1])
+                return arr[mid];
+            }
+            
+            else if(mid==n-1)
+            {
+                if(arr[mid]>arr[mid-1])
+                return arr[mid];
+            }
+            else if(arr[mid]>arr[mid+1] && arr[mid]>arr[mid-1])
+            {
+                return arr[mid];
+            }
+            else if(arr[mid]>arr[mid+1] && arr[mid]<arr[mid-1])
+            {
+                hi=mid-1;
+            }
+            else if(arr[mid]<arr[mid+1] && arr[mid]>arr[mid-1])
+            {
+                lo=mid+1;
+            }
+        }
+        return -1;
+    }
+}
